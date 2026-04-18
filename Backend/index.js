a639541e7b3685cc9f2d8bb3b807e503a9ee5234
Rahul <rahulsingh835789@gmail.com>
2026-04-19 00:00:45 +0530

    add auth controller , route and authmiddleware

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
-// import authRoutes from "./routes/authRoutes.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -16,12 +16,8 @@ app.use(express.json());
 app.use(cors());
 
 // Routes
-// app.use("/api/auth", authRoutes);
+app.use("/api/auth", authRoutes);
 
-// Test route
-app.get("/", (req, res) => {
-  res.send("API Running...");
-});
 
 const PORT = process.env.PORT || 5000;
 
